e66f0e5c0578de79ffdd5ddd2eaabffc4b54a71b
Harvey <har2vey@gmail.com>
2016-04-26 03:54:33 +0800

    Updated form success rendering

--- js/contact_me.js
+++ js/contact_me.js
@@ -6,7 +6,46 @@ $(function() {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            window.location = 'http://heavenlyblush.buatbuat.com/success.html';
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email
+                },
+                cache: false,
+                success: function() {
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                    $('#section8').empty();
+                    $('#section8').append('<img src="img/Heavenly-Blush-12.jpg" class="imgFull animated fadeIn"/>');
+                },
+            })
         },
         filter: function() {
             return $(this).is(":visible");
