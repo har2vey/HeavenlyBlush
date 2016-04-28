7060cbc9d7799e086a7c389b786f2645c4246958
Harvey <har2vey@gmail.com>
2016-04-29 04:07:05 +0800

    Updated form submission to receive coupon code

--- js/submit.js
+++ js/submit.js
@@ -16,7 +16,7 @@ $(function() {
             var phone = $("input#phone").val();
             var yogurt = $("input#yogurt:checked").val();
             $.ajax({
-                url: "./promo.php",
+                url: "promo.php",
                 type: "POST",
                 dataType: "json",
                 data: {
@@ -32,17 +32,30 @@ $(function() {
                     $('#contactForm').trigger("reset");
                 },
                 error: function(result) {
-                  //alert(JSON.stringify(result));
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry, server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                    $('#section7').empty();
-                    $('#section7').append('<img src="img/Heavenly-Blush-12.jpg" class="imgFull animated fadeIn"/>');
+                  var res = jQuery.parseJSON(JSON.stringify(result));
+                  if (res.responseText == ""){
+                    $('#section7').remove();
+                  }
+                  else{
+                    var code = res.responseText.split('-');
+                    if (code[1]=='alphamart'){
+                      var coupon = code[0][0]+code[0][1]+code[0][2]+'-'+code[0][3]+code[0][4]+code[0][5]+'-'+code[0][6]+code[0][7]+code[0][8]
+                    }else if (code[1]=='indomaret'){
+                      var coupon = code[0][0]+code[0][1]+code[0][2]+'-'+code[0][3]+code[0][4]+'-'+code[0][5]+code[0][6]+code[0][7]
+                    }
+                    //alert('your code: '+code[0]+', please claim at your nearest '+code[1]);
+                      $('#success').html("<div class='alert alert-danger'>");
+                      $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                          .append("</button>");
+                      $('#success > .alert-danger').append("<strong>Sorry, server is not responding. Please try again later!");
+                      $('#success > .alert-danger').append('</div>');
+                      //clear all fields
+                      $('#contactForm').trigger("reset");
+                      $('#section7').empty();
+                      $('#section7').removeClass('contact');
+                      $('#section7').addClass(code[1]);
+                      $('#section7').append('<div id="code" class="fp-tableCell"><h1>'+coupon+'</h1></div>');
+                  }
                 },
             })
         },
